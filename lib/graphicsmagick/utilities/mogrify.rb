8375e4402e8bf05a8ebf2332f092d72e39fc71fc
Chad McGimpsey <chad.mcgimpsey@gmail.com>
2013-08-27 13:36:07 -0600

    moved mogrify into utilities submodule

--- lib/graphicsmagick/utilities/mogrify.rb
+++ lib/graphicsmagick/utilities/mogrify.rb
@@ -1,11 +1,13 @@
 module GraphicsMagick
-	module Mogrify
+	module Utilities
+		module Mogrify
 
-		private
+			private
 
-		# gm mogrify [options] file
-		def build_mogrify_command(file_path)
-			"mogrify #{options_to_str(@command_options)} #{file_path}"
+			# gm mogrify [options] file
+			def build_mogrify_command(file_path)
+				"mogrify #{options_to_str(@command_options)} #{file_path}"
+			end
 		end
 	end
 end

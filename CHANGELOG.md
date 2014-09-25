9e0717678dbeafc4f270b5fea77f3e0aa2fb7822
Chad McGimpsey <chad.mcgimpsey@gmail.com>
2014-09-25 11:13:02 -0600

    Updated scheme to test if GraphicsMagick package is installed

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # GraphicsMagick Changelog
 
+## 1.0.5
+Switching scheme to test if GraphicsMagick is installed on the system.
+
 ## 1.0.0
 Initial release with support for mogrify, convert, and composite utilities, plus a few identify convenience methods.
--- lib/graphicsmagick.rb
+++ lib/graphicsmagick.rb
@@ -1,8 +1,11 @@
+require 'mkmf'
+
 module GraphicsMagick
   
 end
 
-raise StandardError, "Please install GraphicsMagick" unless system("hash gm 2>&-")
+# raise StandardError, "Please install GraphicsMagick" unless system("hash gm 2>&-")
+raise StandardError, "Please install GraphicsMagick" unless find_executable('gm')
 
 require "graphicsmagick/version"
 require "graphicsmagick/image"
